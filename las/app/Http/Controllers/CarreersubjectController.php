8269a7eb777a3a7018b64c10a2d0937a7570e930
FartInhaler <129055537+FartInhaler@users.noreply.github.com>
2024-09-23 09:46:50 +0200

    overige migrations uitvoeren

---
+++ las/app/Http/Controllers/CarreersubjectController.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Carreersubject;
+use App\Http\Requests\StoreCarreersubjectRequest;
+use App\Http\Requests\UpdateCarreersubjectRequest;
+
+class CarreersubjectController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StoreCarreersubjectRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Carreersubject $carreersubject)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Carreersubject $carreersubject)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdateCarreersubjectRequest $request, Carreersubject $carreersubject)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Carreersubject $carreersubject)
+    {
+        //
+    }
+}
---
+++ las/app/Http/Controllers/SubjectgradesController.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Subjectgrades;
+use App\Http\Requests\StoreSubjectgradesRequest;
+use App\Http\Requests\UpdateSubjectgradesRequest;
+
+class SubjectgradesController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StoreSubjectgradesRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Subjectgrades $subjectgrades)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Subjectgrades $subjectgrades)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdateSubjectgradesRequest $request, Subjectgrades $subjectgrades)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Subjectgrades $subjectgrades)
+    {
+        //
+    }
+}
